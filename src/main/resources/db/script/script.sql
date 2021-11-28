940446cc15058c3a0f6017e3f40da39a5d4b195f
Daniel Rodrigues <dffrs@iscte-iul.pt>
2021-11-28 21:58:08 +0000

    Corrected script.sql.

--- src/main/resources/db/script/script.sql
+++ src/main/resources/db/script/script.sql
@@ -1,43 +1,53 @@
-create table if not exists timer_app.User
+-- Project: table
+CREATE TABLE `Project`
 (
-    user_id   int auto_increment
-        constraint `PRIMARY`
-        primary key,
-    user_name varchar(40) not null
-)
-    comment 'Info about the user';
+    `project_id`          int         NOT NULL AUTO_INCREMENT,
+    `project_name`        varchar(50) NOT NULL,
+    `project_description` varchar(200) DEFAULT NULL,
+    `user_id`             int          DEFAULT NULL,
+    PRIMARY KEY (`project_id`),
+    UNIQUE KEY `Project_project_name_uindex` (`project_name`),
+    KEY `Project_User_user_id_fk` (`user_id`),
+    CONSTRAINT `Project_User_user_id_fk` FOREIGN KEY (`user_id`) REFERENCES `User` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE = InnoDB
+  AUTO_INCREMENT = 23
+  DEFAULT CHARSET = utf8mb4
+  COLLATE = utf8mb4_0900_ai_ci COMMENT ='Info about the all projects saved in this db';
 
-create table if not exists timer_app.Project
+-- No native definition for element: Project_User_user_id_fk (index)
+
+-- User: table
+CREATE TABLE `User`
 (
-    project_id          int auto_increment
-        constraint `PRIMARY`
-        primary key,
-    project_name        varchar(50)  not null,
-    project_description varchar(200) null,
-    user_id             int          null,
-    constraint Project_project_name_uindex
-        unique (project_name),
-    constraint Project_User_user_id_fk
-        foreign key (user_id) references timer_app.User (user_id)
-            on update cascade on delete cascade
-)
-    comment 'Info about the all projects saved in this db';
+    `user_id`   int         NOT NULL AUTO_INCREMENT,
+    `user_name` varchar(40) NOT NULL,
+    PRIMARY KEY (`user_id`)
+) ENGINE = InnoDB
+  AUTO_INCREMENT = 22
+  DEFAULT CHARSET = utf8mb4
+  COLLATE = utf8mb4_0900_ai_ci COMMENT ='Info about the user';
 
-create table if not exists timer_app.Working_Time
+-- Working_Time: table
+CREATE TABLE `Working_Time`
 (
-    starting_time datetime default CURRENT_TIMESTAMP not null,
-    time_spent    time                               not null,
-    project_id    int                                not null,
-    constraint Working_Time_Project_project_id_fk
-        foreign key (project_id) references timer_app.Project (project_id)
-            on update cascade on delete cascade
-)
-    comment 'Info about time spent working on a project, including its initial date.';
+    `starting_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `time_spent`    time     NOT NULL,
+    `project_id`    int      NOT NULL,
+    KEY `Working_Time_Project_project_id_fk` (`project_id`),
+    CONSTRAINT `Working_Time_Project_project_id_fk` FOREIGN KEY (`project_id`) REFERENCES `Project` (`project_id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4
+  COLLATE = utf8mb4_0900_ai_ci COMMENT ='Info about time spent working on a project, including its initial date.';
 
-create
-    definer = ultra@localhost procedure timer_app.InsertNewProject(IN projectName varchar(50),
-                                                                   IN projectDescription varchar(200),
-                                                                   IN user varchar(40))
+-- No native definition for element: Working_Time_Project_project_id_fk (index)
+
+-- InsertNewProject(varchar(50),varchar(200),varchar(40)): routine
+CREATE
+    DEFINER = `ultra`@`localhost` PROCEDURE `InsertNewProject`(
+    IN projectName VARCHAR(50),
+    IN projectDescription VARCHAR(200),
+    IN user VARCHAR(40)
+)
 BEGIN
     DECLARE u_id INT DEFAULT -1;
 
@@ -52,10 +62,11 @@ BEGIN
 
 END;
 
-grant execute, alter routine on procedure timer_app.InsertNewProject to ultra@localhost;
-
-create
-    definer = ultra@localhost procedure timer_app.InsertNewUser(IN userName varchar(40))
+-- InsertNewUser(varchar(40)): routine
+CREATE
+    DEFINER = `ultra`@`localhost` PROCEDURE `InsertNewUser`(
+    IN userName VARCHAR(40)
+)
 BEGIN
 
     INSERT INTO User(user_id, user_name)
@@ -65,12 +76,14 @@ BEGIN
 
 END;
 
-grant execute, alter routine on procedure timer_app.InsertNewUser to ultra@localhost;
-
-create
-    definer = ultra@localhost procedure timer_app.SaveTime(IN projectName varchar(50),
-                                                           IN projectDescription varchar(200), IN user varchar(40),
-                                                           IN timeSpent time)
+-- SaveTime(varchar(50),varchar(200),varchar(40),time): routine
+CREATE
+    DEFINER = `ultra`@`localhost` PROCEDURE `SaveTime`(
+    IN projectName VARCHAR(50),
+    IN projectDescription VARCHAR(200),
+    IN user VARCHAR(40),
+    IN timeSpent TIME
+)
 BEGIN
 
     DECLARE p_id INT DEFAULT -1;
@@ -90,7 +103,5 @@ BEGIN
     VALUES (timeSpent, p_id);
 
 END;
-
-grant execute, alter routine on procedure timer_app.SaveTime to ultra@localhost;
 
 
